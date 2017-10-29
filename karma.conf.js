8f92d28735fbe029b5cc8bf0a6096bc86f97e7af
Johannes Kartheininger <kartheininger@users.noreply.github.com>
2017-10-29 13:57:27 +0100

    switched to chrome headless.

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function (config) {
   config.set({
@@ -27,7 +28,17 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['HeadlessChrome'],
+    singleRun: false,
+    customLaunchers: {
+      HeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
   });
 };
